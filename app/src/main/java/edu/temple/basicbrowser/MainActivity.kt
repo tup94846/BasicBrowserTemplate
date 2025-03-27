5f870109561eda85c962b26e88ad83692f10fd5c
tup94846 <150381476+tup94846@users.noreply.github.com>
2025-03-27 12:08:51 -0400

    - Making the android studio trust the url from android manifest - Using the ensure protocol to accept http://

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.basicbrowser
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var goButton: ImageButton
     private lateinit var webView: WebView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,13 +22,26 @@ class MainActivity : AppCompatActivity() {
         urlEditText = findViewById(R.id.urlEditText)
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
 
         // Allow your browser to intercept hyperlink clicks
-        webView.webViewClient = object: WebViewClient() {
+        webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
             }
         }
+        goButton.setOnClickListener {
+            val url = urlEditText.text.toString()
+            val validUrl = ensureProtocol(url)
+            webView.loadUrl(validUrl)
+        }
+    }
 
+    private fun ensureProtocol(url: String): String {
+        return if (url.startsWith("http://") || url.startsWith("https://")) {
+            url
+        } else {
+            "https://$url"
+        }
     }
 }
